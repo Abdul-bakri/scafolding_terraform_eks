fa13b7a48d2c9a52d6b4ec8f7b0048acce4f2335
stephen <66844942+Abdul-bakri@users.noreply.github.com>
2022-10-13 17:47:28 +0100

    Update variables.tf

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,18 +1,27 @@
-variable "region" {
+variable "aws_access_key" {
+  type = string
+  description = "Access key"
+}
+variable "aws_secret_key" {
+  type = string
+  description = "SECRET"
+}
+variable "aws_session_token" {
+  type = string
+  description = "SESSION"
+}
+variable "aws_region" {
   type = string
   description = "region of deployment"
 }
-
 variable "bucket_name" {
   type = string
   description = "name of your state file bucket"
-  default = "eks-state-files"
 }
 
 variable "environment" {
   type = string
   description = "environment name"
-  default = "dev"
 }
 
 variable "project" {
