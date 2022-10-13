284e21817fbb3c533b60a3aed8425010cb6edde7
stephen <66844942+Abdul-bakri@users.noreply.github.com>
2022-10-13 19:07:23 +0100

    Update variables.tf

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,3 +1,22 @@
+#####
+#Credentials
+####
+variable "aws_access_key" {
+  type = string
+  description = "Access key"
+}
+variable "aws_secret_key" {
+  type = string
+  description = "SECRET"
+}
+variable "aws_session_token" {
+  type = string
+  description = "SESSION"
+}
+variable "aws_region" {
+  type = string
+  description = "region of deployment"
+}
 # tflint-ignore: terraform_unused_declarations
 variable "cluster_name" {
   description = "Name of cluster - used by Terratest for e2e test automation"
