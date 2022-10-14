9c0c877e28f4a1999a3e04801e271a6790141e5b
stephen <66844942+Abdul-bakri@users.noreply.github.com>
2022-10-14 17:10:22 +0100

    Add files via upload

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,1 +1,19 @@
-variables.tf
+#####
+#Credentials
+####
+variable "aws_access_key" {
+  type = string
+  description = "Access key"
+}
+variable "aws_secret_key" {
+  type = string
+  description = "SECRET"
+}
+variable "aws_session_token" {
+  type = string
+  description = "SESSION"
+}
+variable "aws_region" {
+  type = string
+  description = "region of deployment"
+}
